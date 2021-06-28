90f9b6d8f40265a31f8a0eecd040d7eaa5017918
russell <russelgal@gmail.com>
2021-06-28 05:55:31 +0500

    fix

--- examples/shared/store.js
+++ examples/shared/store.js
@@ -1,5 +1,7 @@
 import { createResource, createRoot } from "solid-js";
+import fetch from "node-fetch";
 
+globalThis.fetch || (globalThis.fetch = fetch);
 export const store = createRoot(() => {
   let [r] = createResource(() => fetch("https://dog.ceo/api/breeds/list/all").then(r => r.json));
   return { dog: r() };
